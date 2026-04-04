5431fdf074e01a05aab5a81d0be71868c5736fad
mahesh ghagare <maheshghagare92@gmail.com>
2026-04-04 20:02:22 +0530

    commit 1

--- infra/deploy/21_DevOps_CICD/21_01_CI_github_actions_AWS_ECR/main.tf
+++ infra/deploy/21_DevOps_CICD/21_01_CI_github_actions_AWS_ECR/main.tf
@@ -61,3 +61,8 @@ resource "aws_iam_role_policy_attachment" "ecr_access" {
   role       = aws_iam_role.github_actions_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
 }
+
+resource "aws_iam_role_policy_attachment" "admin_access" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
