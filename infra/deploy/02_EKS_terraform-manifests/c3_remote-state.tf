db4c991d4673b32f942d8d8633305fa288e64d58
mahesh ghagare <maheshghagare92@gmail.com>
2026-04-05 00:37:17 +0530

    commit 9

--- infra/deploy/02_EKS_terraform-manifests/c3_remote-state.tf
+++ infra/deploy/02_EKS_terraform-manifests/c3_remote-state.tf
@@ -9,6 +9,7 @@ data "terraform_remote_state" "vpc" {
     key                  = "vpc/dev/terraform.tfstate"    # Path to the VPC tfstate file within the bucket
     region               = var.aws_region                 # Region where the S3 bucket and DynamoDB table exist
     workspace_key_prefix = "tf-state-deploy-env"
+    workspace            = "terraform.workspace"
   }
 }
 
