057f135546b5bfce881d92f5860e0b4db76b35f6
mahesh ghagare <maheshghagare92@gmail.com>
2026-08-15 23:15:13 +0530

    code updated at 11 15 15

---
+++ infra/deploy/03_EKS_terraform-manifests/pending/argocd.tf
@@ -0,0 +1,8 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  namespace        = "argocd"
+  create_namespace = true
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  version          = "9.5.0"
+}
