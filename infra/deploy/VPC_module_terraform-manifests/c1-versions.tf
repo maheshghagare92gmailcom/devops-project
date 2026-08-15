43c0b7f1e49a435f1c01c14e86ec5076e7b8e12d
mahesh ghagare <maheshghagare92@gmail.com>
2026-08-15 19:36:09 +0530

    code updated at 7 36 15

---
+++ infra/deploy/VPC_module_terraform-manifests/c1-versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+  }
+  # Remote Backend
+  backend "s3" {
+    bucket               = "tfstate-dev-us-east-1-lqvonw"
+    key                  = "vpc/dev/terraform.tfstate"
+    workspace_key_prefix = "tf-state-deploy-env"
+    region               = "us-east-1"
+    encrypt              = true
+    # use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
