586563a54513e4ad9d2b4860c30d4806c39c2ab9
mahesh ghagare <maheshghagare92@gmail.com>
2026-08-27 23:01:22 +0530

    updated code

--- infra/deploy/21_DevOps_CICD/21_01_CI_github_actions_AWS_ECR/outputs.tf
+++
@@ -1,9 +0,0 @@
-# output "ecr_repository_url" {
-#   description = "ECR Repository URL"
-#   value       = aws_ecr_repository.ui.repository_url
-# }
-
-output "github_role_arn" {
-  description = "IAM Role ARN for GitHub Actions"
-  value       = aws_iam_role.github_actions_role.arn
-}
